1b7d51375fdd6abccd832052247f8ca5e9e92f38
Chloe <cguttwebdesign@gmail.com>
2019-01-13 13:14:32 +0000

    initial commit

---
+++ src/components/dropdown.vue
@@ -0,0 +1,108 @@
+<template>
+    <div>
+        <h1>{{ header }}</h1>
+        <p>{{ info }}</p>
+        <b-form-select v-model="selected" class="col-3 py-2">
+            <option value="">Please select a method</option>
+            <option v-for="method in arrayMethods" :value="method.method" :key="method"> {{ method.name }}</option>
+        </b-form-select>
+        <p class="explain">{{ selected }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+// data needs to be a function and return an object
+    data() {
+        return {
+        header: 'Array Helper',
+        info: 'Use the dropdown below to learn about various array methods',
+        selected: '',
+        arrayMethods: [
+            { 
+              name: 'concat',
+              method: 'This method will join two or more arrays together' 
+            },
+            {
+              name: 'copyWithin',
+              method: 'copies part of an array to another location in the same array'
+            },
+            {  
+              name: 'every',
+              method: 'tests whether all elements in array pass the test given by function provided. This returns true for any condition on an empty array.' 
+            },
+            { 
+              name: 'filter',
+              method: 'creates a new array with all elements that pass the test given in the provided function' 
+            },
+            { 
+              name: 'find',
+              method: 'find given value within an array' 
+            },
+            { 
+              name: 'includes',
+              method: 'checks whether given value is included in selected array outputs a Boolean value' 
+            },
+            { 
+              name: 'indexOf',
+              method: 'gives an index value of given element will return -1 if element is not present' 
+            },
+            { 
+              name: 'join',
+              method: 'join the elements in an array' 
+            },
+            { 
+              name: 'lastIndexOf',
+              method: 'array is searched backwards it gives the last index value of given element will return -1 if not present' 
+            },
+            { 
+              name: 'map',
+              method: 'this is an alternative to using sort if there is a number of elements' 
+            },
+            { 
+              name: 'pop',
+              method: 'remove the last element from an array' 
+            },
+            { 
+              name: 'push',
+              method: 'add element to the end of an array' 
+            },
+            { 
+              name: 'reduce',
+              method: 'reduces an array to a single value' 
+            },
+            { 
+              name: 'shift',
+              method: 'removes the first element of an array' 
+            },
+            { 
+              name: 'slice',
+              method: 'returns a shallow copy of portion of an array into a new array object. It does not modify the existing array' 
+            },
+            { 
+              name: 'sort',
+              method: 'sorts the elements of array in place and returns array. If using sort elements are converted to strings and compared in unicode point value so numbers will take priority' 
+            },
+            { 
+              name: 'splice',
+              method: 'changes content of an array by removing existing elements and/or adding new elements' 
+            },
+            { 
+              name: 'unshift',
+              method: 'add 1 or more elements to the front of an array' 
+            },
+          ],
+    }
+    }
+}
+</script>
+
+<style scoped>
+  .explain { 
+    color:  #41B883;
+    font-weight: 600;
+    padding-top: 1rem;
+    }
+</style>
+
+
---
+++ src/components/examplebox.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="example mt-4">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    .example {
+        border: 1px solid #35495E;
+        min-height: 200px;
+        margin: auto;
+        width: 80%;
+    }
+</style>
+
